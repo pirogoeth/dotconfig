8f9823d113c1a1e169c158235f3ced62730d23d8
Sean Johnson <sean.johnson@maio.me>
2017-01-11 23:21:46 -0600

    Add dotconfig-util.sh

---
+++ bin/dotconfig-util.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+#
+# dotconfig-util.sh - bash utilities for managing dotconfig
+#
+
+function reapply-dotconfig () {
+    args=${*}
+
+    if [ ! -d "${HOME}/.dotconfig" ] ; then
+        echo " [!] dotconfig playbook does not exist at ~/.dotconfig"
+        return 1
+    fi
+
+    pushd ${HOME}/.dotconfig 2>&1 1>/dev/null
+    ansible-playbook -i inventory/hosts.txt ${args} site.yml
+    popd 2>&1 1>/dev/null
+}
+
+function modify-dotconfig-role () {
+    role=$1;    shift
+    args=${*}
+
+    if [ ! -d "${HOME}/.dotconfig" ] ; then
+        echo " [!] dotconfig playbook does not exist at ~/.dotconfig"
+        return 1
+    fi
+
+    vim ${HOME}/.dotconfig/roles/internal/${role}
+}
